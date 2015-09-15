178e3df60dd5f3e3406eeb6a377a7d9619f5537f
HK Zhang <yxzhk@hotmail.com>
2015-09-15 19:54:35 +0800

    mongo db
    mongo db

--- Mongo/MongoDB.py
+++ Mongo/MongoDB.py
@@ -1,6 +1,9 @@
 ﻿import pymongo
 from pymongo import MongoClient
+from bson.code import Code
+import random as ran
 import sys
+import csv
 import json
 
 client = MongoClient('mongodb://192.168.230.129:27017/')
@@ -72,12 +75,94 @@ def aggregateAdvBy():
     for doc in results:
         print doc
     
-                   
+def mapReduce():
+    db = client.Corpus
+    tweets = db.tweets
+    map = Code("function() {emit(this.via,1);}")
+    reduce = Code("""function(key, values) { 
+                        var res=0;
+                        values.forEach(function(v){res +=1})
+                        return {count: res};
+                        }""")
+
+    result = tweets.map_reduce(map,reduce,"via_count",query={"sentiment":2},limit =10)
+    print result
+
+    for doc in db.via_count.find():
+        print(doc)
+ 
+def insertData():
+    db = client.test
+    games = db.games
+    players = ["LeBron James",
+           "Allen Iverson",
+           "Kobe Bryant",
+           "Rick Barry",
+           "Dominique Wilkins",
+           "George Gervin",
+           "Dwyane Wade",
+           "Jerry West",
+           "Pete Maravich",
+           "Carmelo Anthony"]
+    for x in range(100):
+        games.insert({"player":players[ran.randint(0,9)],
+                      "points":ran.randint(0,100)})
+ 
+def aggregateMR():
+    db = client.test
+    games = db.games
+    map = Code("function() {emit(this.player,this.points);}")
+    reduce = Code("""function(key, values) { 
+                        var explain={total:Array.sum(values),
+                                    max:Math.max.apply(Math,values),
+                                    min:Math.min.apply(Math,values),
+                                    avg:Array.sum(values)/values.length}
+                        return explain;
+                        }""")
+    result = games.map_reduce(map,reduce,"_result")
+    print(result)
+    for p in db["_result"].find():
+        print(p)            
+ 
+def wordCloud():
+    db = client.Corpus
+    tweets = db.tweets
+    map = Code("""function() {
+                    this.text.split(' ').forEach(
+                        function(word){
+                            var txt = word.toLowerCase();
+                            if(!(/^@/).test(txt) && txt.length > 3 && !(/^http/).test(txt)){
+                            emit(txt,1)
+                            }
+                        }
+                    )
+    }""")
+
+    reduce = Code("""function(key,values){
+                        var res = 0;
+                        values.forEach(function(v){res +=1 })
+                        return {count: res};
+    }""")
+
+    result = tweets.map_reduce(map,reduce,"TweetWords",query = {"sentiment":4})
+    with open(r"F:\PY\data\tweetsData.csv","wb") as f:
+        f_csv = csv.writer(f,delimiter = ',')
+        f_csv.writerow(["text","size"])
+
+        for doc in db.TweetWords.find().sort("value",direction = -1).limit(50):
+            f_csv.writerow([doc["_id"],doc["value"]["count"]+30])
+            print doc
+          
+                  
 def main():
     #groupBy()
     #aggregateBy()
     #pipelineBy()
-    aggregateAdvBy()
+    #aggregateAdvBy()
+    #mapReduce()
+    #insertData()
+    #aggregateMR()
+    wordCloud()
 
 if __name__ == "__main__":
     main()
