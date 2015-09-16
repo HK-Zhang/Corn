5d866ccf723f6486744029eb822540ae116635e8
Zhang HeKe <yxzhk@hotmail.com>
2015-09-16 17:39:11 +0800

    Create MSSQL.py
    MSSQL

---
+++ Others/MSSQL.py
@@ -0,0 +1,35 @@
+import pymssql
+import _mssql
+
+
+class MSSQL:
+    def __init__(self,host,user,pwd,db):
+        self.host = host
+        self.user = user
+        self.pwd = pwd
+        self.db = db
+
+    def __GetConnect(self):
+        if not self.db:
+            raise(NameError,"No Database Information")
+        self.conn = pymssql.connect(host=self.host,user=self.user,password=self.pwd,database=self.db,charset="utf8")
+        #self.conn = pymssql.connect(host=self.host,database=self.db,charset="utf8",trusted=True)
+        cur = self.conn.cursor()
+        if not cur:
+            raise(NameError,"Failed to connect database")
+        else:
+            return cur
+
+    def ExecQuery(self,sql):
+        cur = self.__GetConnect()
+        cur.execute(sql)
+        resList = cur.fetchall()
+
+        self.conn.close()
+        return resList
+
+    def ExecNonQuery(self,sql):
+        cur = self.__GetConnect()
+        cur.execute(sql)
+        self.conn.commit()
+        self.conn.close()
