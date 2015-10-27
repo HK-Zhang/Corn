2be76befc92d1acfe1d0bd4807ddbf01e378c952
HK Zhang <yxzhk@hotmail.com>
2015-10-27 22:12:00 +0800

    numpy basic example
    numpy basic example

---
+++ Toolkit/NumpyBasic.py
@@ -0,0 +1,60 @@
+﻿import numpy as np
+import sys
+
+def dTypeTest():
+    #Using dictionaries. Two fields named ‘gender’ and ‘age’:
+    student=np.dtype({'names':['name', 'age', 'weight'],'formats':['S32', 'i','f']}, align=True)
+    a=np.array([('zhang',65,123.5),('wang',23,122.5)],dtype=student)
+    print a
+    #Using array-scalar type:
+    a=np.array([1,2],dtype=np.dtype(np.int16))
+    print a
+
+    #Structured type, one field name 'f1', containing int16:
+    #a=np.array([1,2],dtype=np.dtype([('f1', np.int16)]))
+    #http://docs.scipy.org/doc/numpy/reference/generated/numpy.dtype.html
+    #http://numpy-discussion.10968.n7.nabble.com/Structured-array-inititialization-weirdness-td23335.html
+    a=np.array([1,2]).view(np.dtype([('f1', np.int16)])) 
+    print a
+    a=np.array([1,2]).view(np.dtype([('f1', np.int32)])) 
+    print a
+    a=np.array([(1,),(2,)],dtype=np.dtype([('f1', np.int16)]))
+    print a
+    #below two lines of code is not working as [1,2] is not a list of tuple
+    #a=np.array([1,2],dtype=np.dtype([('f1', np.int16)]))
+    #a=np.array([(1),(2,)],dtype=np.dtype([('f1', np.int16)]))
+
+    #Structured type, one field named ‘f1’, in itself containing a structured type with one field:
+    dt=np.dtype([('f1', [('f1', np.int32)])])
+    a=np.array([1,2]).view(dt) 
+    print a
+    a=np.array([((1,),),((2,),)],dtype=dt)
+    print a
+    #below two lines of code is not working as (1,) is not same as the structure of dtype declared
+    #a=np.array([(1,),(2,)],dtype=dt)
+
+    #Structured type, two fields: the first field contains an unsigned int, the second an int32
+    dt=np.dtype([('f1', np.uint), ('f2', np.int32)])
+    a=np.array([(1,2),(3,4)],dtype=dt)
+    print a
+
+    #Using array-protocol type strings:
+    dt=np.dtype([('a','f8'),('b','S10')])
+    a=np.array([(3.14,'pi'),(2.17,'e')],dtype=dt)
+    print a
+
+    #Using comma-separated field formats. The shape is (2,3):
+    dt=np.dtype("i4, (2,3)f8")
+    a=np.array([(1,[[1,2,3],[4,5,6]]),(2,[[4,5,6],[1,2,3]])],dtype=dt)
+    print a
+
+    #Using tuples. int is a fixed type, 3 the field’s shape. void is a flexible type, here of size 10
+    dt=np.dtype([('hello',(np.int,3)),('world',np.void,10)])
+    a=np.array([([1,2,3],'this is a')],dtype=dt)
+    print a
+
+def main():
+    dTypeTest()
+
+if __name__ == "__main__":
+    main()
---
+++ Toolkit/TupleTest.py
@@ -0,0 +1,66 @@
+﻿#不可变序列-----元组 tuple
+		
+#元组和列表十分相似,元组和字符串一样都是不可变的。
+		
+#元组由不同的元素组成，每个元素可以存储不同类型的数据，例如
+#字符串、数字和元组
+		
+#元组通常代表一行数据，而元组中的元素则代表不同的数据项
+		
+#创建元组，不定长，但一旦创建后则不能修改长度
+		
+#空元组
+tuple_name = ()
+print tuple_name
+
+#如果创建的元组只有一个元素，那么该元素后面的逗号是不可忽略的
+print (4),(4,)
+
+user = ('01','02','03','04')
+#添加元组
+user = (user,'05')
+print user
+
+#去重
+a=set((2,2,2,4,4))
+print a
+
+#访问元组
+print user[0]
+
+#不可修改元素
+user=(1,2,3)
+#user[0]=2
+		
+		
+#解包
+user = (1,2,3)
+a,b,c = user
+print a,b,c
+
+##访问二元元组
+user1 = (1,2,3)
+user2 = (4,5,6)
+user = (user1,user2)
+print user[0][0]
+			
+#元组的遍历
+#range([start],stop,[,step]) 返回一个递增后者递减的 数字 列表
+		
+for item in range(len(user)):
+	print user[item]
+		    
+#二元元组的访问
+for i in range(len(user)):
+	for j in range(len(user[i])):
+		print 'user['+str(i)+']['+str(j)+']=',user[i][j]
+		   
+#使用map()实现遍历	
+#map(function_name,sequence[,sequence...])
+		
+#返回 function处理后的列表
+#sequence 元组或列表
+		
+for item in map(None,user):
+	for i in item:
+		print i
