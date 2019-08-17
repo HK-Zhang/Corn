560357434bfb1b161b854a3d268430ca4e1476de
yxzhk@hotmail.com <yxzhk@hotmail.com>
2019-08-17 21:47:46 +0800

    flask web

---
+++ FlaskWeb/config.py
@@ -0,0 +1,40 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class config:    
+    SECRET_KEY = os.environ.get('SECRET_KEY') or """S8LApqTclxj9lfVB7HJP
+KJxI2Wnh3kST1Fux9hms
+P3i62M8S2mjis96sz0h3
+REWiBG0XGX5crKVhvccn
+lBra0dBP4yWjgB1F0IWb
+uBLBFjCCzldiUnOGsmir
+V5XmJf1GRBhWc1SOtQMr
+5IiAofvp3sf47f5w0ZAa
+KWHkMEWm9BNEC6ssa3gA
+CmSwywTaWjfUm1DHzHgx
+4iYWXXvcm9ExGurpCtP6
+SYJP4GZVdQ3vj9O25ED2"""
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(config):    
+    DEBUG = True    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'dev')
+
+class TestingConfig(config):    
+    TESTING = True    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'test')
+
+class ProductionConfig(config):    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+config = {    
+    'development': DevelopmentConfig,    
+    'testing': TestingConfig,    
+    'production': ProductionConfig,    
+    'default': DevelopmentConfig
+}
